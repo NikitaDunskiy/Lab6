cd2b330d0ac6e814720aa981adc5b7035dd4bbda
NikitaDunskiy <nikita.dunskiy@gmail.com>
2020-04-14 12:24:41 +0300

    alpha version

---
+++ src/bsu/rfe/java/group8/lab6/Dunskiy/var4/Field.java
@@ -0,0 +1,35 @@
+package bsu.rfe.java.group8.lab6.Dunskiy.var4;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+@SuppressWarnings("serial")
+public class Field extends JPanel {
+    // Флаг приостановленности движения
+    private boolean paused;
+    // Динамический список скачущих мячей
+    private ArrayList<BouncingBall> balls = new ArrayList<BouncingBall>(10);
+    // Класс таймер отвечает за регулярную генерацию событий ActionEvent
+// При создании его экземпляра используется анонимный класс,
+// реализующий интерфейс ActionListener
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        public void actionPerformed(ActionEvent ev) {
+// Задача обработчика события ActionEvent - перерисовка окна
+            repaint();
+        }
+    });
+    // Конструктор класса BouncingBall
+    public Field() {
+// Установить цвет заднего фона белым
+        setBackground(Color.WHITE);
+// Запустить таймер
+        repaintTimer.start();
+    }
+    // Унаследованный от JPanel метод перерисовки компонента
+
+}
