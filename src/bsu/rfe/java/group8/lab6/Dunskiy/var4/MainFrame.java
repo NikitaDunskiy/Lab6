4a2e47f7d792f8c6ffaea929d72df293bf0825e1
NikitaDunskiy <nikita.dunskiy@gmail.com>
2020-04-14 12:33:45 +0300

    menu buttons

--- src/bsu/rfe/java/group8/lab6/Dunskiy/var4/MainFrame.java
+++ src/bsu/rfe/java/group8/lab6/Dunskiy/var4/MainFrame.java
@@ -28,18 +28,45 @@ public class MainFrame extends JFrame {
         setLocation((kit.getScreenSize().width - WIDTH)/2,
                 (kit.getScreenSize().height - HEIGHT)/2);
 // Установить начальное состояние окна развѐрнутым на весь экран
+        
         setExtendedState(MAXIMIZED_BOTH);
 // Создать меню
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         JMenu ballMenu = new JMenu("Мячи");
-
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+// Ни один из пунктов меню не являются
+// доступными - сделать доступным "Паузу"
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
         menuBar.add(ballMenu);
-
+        ballMenu.add(addBallAction);
         JMenu controlMenu = new JMenu("Управление");
         menuBar.add(controlMenu);
-
-
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
 // Добавить в центр граничной компоновки поле Field
         getContentPane().add(field, BorderLayout.CENTER);
     }
