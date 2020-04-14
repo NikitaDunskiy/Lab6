1520301a5a357903b72ec1ce284a6b3d4d73f72d
NikitaDunskiy <nikita.dunskiy@gmail.com>
2020-04-14 12:29:15 +0300

    Implemented class Field

--- src/bsu/rfe/java/group8/lab6/Dunskiy/var4/Field.java
+++ src/bsu/rfe/java/group8/lab6/Dunskiy/var4/Field.java
@@ -31,5 +31,45 @@ public class Field extends JPanel {
         repaintTimer.start();
     }
     // Унаследованный от JPanel метод перерисовки компонента
-
+    public void paintComponent(Graphics g) {
+// Вызвать версию метода, унаследованную от предка
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+// Последовательно запросить прорисовку от всех мячей из списка
+        for (BouncingBall ball: balls) {
+            ball.paint(canvas);
+        }
+    }
+    // Метод добавления нового мяча в список
+    public void addBall() {
+//Заключается в добавлении в список нового экземпляра BouncingBall
+// Всю инициализацию положения, скорости, размера, цвета
+// BouncingBall выполняет сам в конструкторе
+        balls.add(new BouncingBall(this));
+    }
+    // Метод синхронизированный, т.е. только один поток может
+// одновременно быть внутри
+    public synchronized void pause() {
+// Включить режим паузы
+        paused = true;
+    }
+    // Метод синхронизированный, т.е. только один поток может
+// одновременно быть внутри
+    public synchronized void resume() {
+// Выключить режим паузы
+        paused = false;
+// Будим все ожидающие продолжения потоки
+        notifyAll();
+    }
+    // Синхронизированный метод проверки, может ли мяч двигаться
+// (не включен ли режим паузы?)
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+// Если режим паузы включен, то поток, зашедший
+// внутрь данного метода, засыпает
+            wait();
+        }
+    }
 }
+
